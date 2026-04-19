ed2d73faab3c45281a9e8c44fcf6e8cb18942108
Prathamesh Kakde <kakdeprathamesh860@gmail.com>
2026-04-19 12:20:48 +0530

    chore: setup MVC package structure

--- src/main/java/com/example/attendance/config/SecurityConfig.java
+++ src/main/java/com/example/attendance/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.attendance;
+package com.example.attendance.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
